e39872b01221a6b7ff3074b4dfb9ac4ad730428b
Dorge47 <Dorge47@users.noreply.github.com>
2021-05-14 02:13:15 -0700

    Add orb guarantee counter
    I was sleep-deprived while writing every line of this code, will check for errors in the morning

--- application.js
+++ application.js
@@ -1,3 +1,17 @@
+function startup() {
+    document.getElementById('rollsForm').style.display = 'none';
+    document.getElementById('orbsForm').style.display = 'none';
+};
+function showRollsForm(whichForm) {
+    if (whichForm) {
+        document.getElementById('rollsForm').style.display = 'block';
+        document.getElementById('orbsForm').style.display = 'none';
+    }
+    else {
+        document.getElementById('orbsForm').style.display = 'block';
+        document.getElementById('rollsForm').style.display = 'none';
+    };
+};
 function findRolls() {
     percentList = [];
     chanceList = [0];
@@ -29,15 +43,47 @@ function findRolls() {
 	rolls = 0;
     if (!(actualChance < (desiredPercent)/100)) {  // We already have the desired chance
         rolls = 1;
-    }
+    };
 	while (actualChance < (desiredPercent)/100) {
 		rolls++;
 		chanceList[rolls] = percentList[Math.floor(rolls/frequency)]/100;
 		cumulativeChance[rolls] = 1 - ((1 - cumulativeChance[rolls-1]) * (1 - chanceList[rolls]));
 		actualChance = cumulativeChance[rolls];
 	};
-    document.getElementById('result').innerText = rolls;
+    document.getElementById('rollsResult').innerText = rolls;
 	return;
+};
+function findOrbs() {
+    if (document.getElementById('summons').value != '' && !isNaN(document.getElementById('summons').value)) {
+        currentSummons = parseFloat(document.getElementById('summons').value);
+    }
+    else {
+        alert('Input a valid number of summons');
+        return 0;
+    };
+    if (currentSummons < 0 || currentSummons > 40) {
+        alert('Invalid number of summons');
+        return 0;
+    };
+    summonsRemaining = 40 - currentSummons;
+    orbsRemaining = Math.floor(summonsRemaining/5) * 20;
+    switch (summonsRemaining % 5) {
+        case 1:
+            orbsRemaining += 5;
+            break;
+        case 2:
+            orbsRemaining += 9;
+            break;
+        case 3:
+            orbsRemaining += 13;
+            break;
+        case 4:
+            orbsRemaining += 17;
+            break;
+        default:
+            break;
+    }
+    document.getElementById('orbsResult').innerText = orbsRemaining + " orbs until guarantee (assuming you roll full multis)"
 };
 function toggleCheckBox() {
     if (document.getElementById('nopity').checked) {
